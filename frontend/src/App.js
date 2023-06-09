b9c144a47b15f7124cffbfdd1c15693abe884972
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2023-06-09 11:11:46 +0530

    Create and MyListedItem added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Spinner } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Marketplace from './contracts/Marketplace.json';
+import NFT from './contracts/NFT.json';
+import Truflation from './contracts/TruflationTester.json';
+import VerseToken from './contracts/VerseTest.json';
+import Navbar from './components/Navbar';
+import Create from "./components/Create";
+import MyListedItems from "./components/MyListedItem";
+import { useState } from 'react';
+const { ethers } = require("ethers");
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [account, setAccount] = useState(null);
+  const [marketplace, setMarketplace] = useState(null);
+  const [nft, setNft] = useState(null);
+  const [truflation, setTruflation] = useState(null);
+  const [verseToken, setVerseToken] = useState(null);
+
+  const web3Handler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0]);
+    //Get Provider from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    //Set up signer
+    const signer = provider.getSigner();
+    
+    loadContracts(signer);
+  }
+
+  const loadContracts = async (signer) => {
+    const marketplace = new ethers.Contract("0x9cA97c2f873253510b886fd45847297656ec2950", Marketplace.abi, signer);
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract("0x619f56373ea848795413e4B106C311d62aCD6F25", NFT.abi, signer);
+    setNft(nft);
+    const truflation = new ethers.Contract("0x211DC534C376CFEB44B4B882b6bF6F153e8DB561", Truflation.abi, signer);
+    setTruflation(truflation);
+    const verseToken = new ethers.Contract("0x37D4203FaE62CCd7b1a78Ef58A5515021ED8FD84", VerseToken.abi, signer);
+    setVerseToken(verseToken);
+    setLoading(false);
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar web3Handler={web3Handler} account={account} />
+        {loading ? (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+        <Spinner animation="border" style={{ display: 'flex' }} />
+        <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
+      </div>
+      ):(
+      <Routes>
+        {/* <Route path="/" element={
+          <Home marketplace={marketplace} nft={nft} />
+        }/> */}
+        <Route path="/create" element={<Create marketplace={marketplace} nft={nft} truflation={truflation} verseToken={verseToken}/>}/>
+        <Route path="/my-listed-items" element={<MyListedItems marketplace={marketplace} nft={nft} account={account}/>}/>
+        {/* <Route path="/my-purchases" element={<MyPurchases marketplace={marketplace} nft={nft} account={account}/>}/> */}
+      </Routes>
+      )}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,47 @@
+import {Link} from 'react-router-dom'
+import {Navbar,Nav,Button,Container,Stack} from 'react-bootstrap'
+import Logo from './Logo01.jpeg'
+
+const Navigation =({web3Handler,account})=>{
+    return(
+        <Navbar expand="lg" variant="dark" bg="primary" style={{ backgroundColor: "blue" }}>
+  <Container>
+    <Navbar.Brand href="">
+      <img src={Logo} alt="logo" width="60" height="60" className="d-inline-block align-top" />
+      &nbsp;
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className='me-auto'>
+        <Nav.Link as={Link} to="/" style={{ color: "white", fontWeight: "bold" }}><Button variant="outline-light">Home</Button></Nav.Link>
+        <Nav.Link as={Link} to="/create" style={{ color: "white", fontWeight: "bold" }}><Button variant="outline-light">Create</Button></Nav.Link>
+        <Nav.Link as={Link} to="/my-listed-items" style={{ color: "white", fontWeight: "bold" }}><Button variant="outline-light">My Insurance</Button></Nav.Link>
+        <Nav.Link as={Link} to="/my-purchases" style={{ color: "white", fontWeight: "bold" }}><Button variant="outline-light">Profile</Button></Nav.Link>
+      </Nav>
+      <Nav>
+        {account ? (
+          <Nav.Link
+            href={`https://etherscan.io/address/${account}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className='button nav-button btn-sm mx-4'
+            style={{ color: "white", fontWeight: "bold" }}
+          >
+            <Button variant="outline-light" style={{backgroundColor:"green",fontWeight: "bold"}}>
+              {account.slice(0, 6)}...{account.slice(-4)}
+            </Button>
+          </Nav.Link>
+        ) : (
+          <Button variant="outline-light" onClick={web3Handler} style={{ color: "yellow", fontWeight: "bold" }}>
+            Connect Wallet
+          </Button>
+        )}
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+    )
+}
+
+export default Navigation;
